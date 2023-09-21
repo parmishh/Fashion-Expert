4a028e079cae314c271989c835cfd416afe40928
paramjit singh <91942072+parmishh@users.noreply.github.com>
2023-09-21 19:22:31 +0530

    Update products-data.js
    Dummy Product data added

--- products-data/products-data.js
+++ products-data/products-data.js
@@ -54,6 +54,13 @@ var products = [
         description:
             "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
         price: 99
+    }),
+     new Product({
+        imagePath: "http://via.placeholder.com/200x200/grey?text=Product",
+        title: "Product 7",
+        description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        price: 99
     })
     
 ]
